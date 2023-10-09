e9bb3d0ae9e7e51c9fe958aff5824497ef526d18
onogod <144659993+onogod@users.noreply.github.com>
2023-10-09 21:33:34 +0900

    Add files via upload

---
+++ kadai005ja/bmi.js
@@ -0,0 +1,19 @@
+let userHeight
+
+userHeight= 1.7;
+console.log(userHeight);
+
+let userWeight
+
+userWeight= 68;
+console.log(userWeight);
+
+
+console.log( userHeight * userHeight );
+
+console.log( userWeight / 2.8899999999999997);
+
+let userBmi
+
+userBmi= 23.529411764705884;
+console.log(23.529411764705884);
