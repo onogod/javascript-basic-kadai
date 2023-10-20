98a6a131f61f0179a47af2d4b285d923ede17f83
onogod <144659993+onogod@users.noreply.github.com>
2023-10-20 13:07:16 +0900

    Add files via upload

---
+++ kadai021js/3event.js
@@ -0,0 +1,10 @@
+const bTn = document.getElementById('btn');
+
+
+ bTn.addEventListener ('click',()=>{
+    setTimeout(() => {
+    document.getElementById('text')
+   text.textContent = 'ボタンをクリックしました';
+  },2000);
+
+});  
